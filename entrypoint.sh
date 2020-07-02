a5f059782dbcf99945b1d90be793615b67acaf49
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-07-02 15:33:32 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-# переносим значения переменных из текущего окружения
-env | while read -r LINE; do  # читаем результат команды 'env' построчно
-    # делим строку на две части, используя в качестве разделителя "=" (см. IFS)
-    IFS="=" read VAR VAL <<< ${LINE}
-    # удаляем все предыдущие упоминания о переменной, игнорируя код возврата
-    sed --in-place "/^${VAR}/d" /etc/security/pam_env.conf || true
-    # добавляем определение новой переменной в конец файла
-    echo "${VAR} DEFAULT=\"${VAL}\"" >> /etc/security/pam_env.conf
-done
+echo "Docker container has been started"
 
-# Запуск cron
-cron -f 
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
-exec "$@"
+# Настройка расписания планировщика
+echo "SHELL=/bin/bash
+BASH_ENV=/container.env
+*/15 * * * * /start.sh >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
