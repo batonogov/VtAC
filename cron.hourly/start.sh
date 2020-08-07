9793c30f489499a03aaf4e5bca634d660a105e91
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-07 17:10:30 +0300

    Добавлены метки времени

--- cron.hourly/start.sh
+++ cron.hourly/start.sh
@@ -4,6 +4,7 @@ mkdir /tmp/lan
 
 until mount -t cifs $CIFS_PATH /tmp/lan -o user=$CIFS_USER,password=$CIFS_PASS
 do
+    echo $(date)
     sleep 60
 done
 
@@ -11,7 +12,9 @@ cd /tmp/lan
 
 until python3 /convert.py
 do
+    echo $(date)
     sleep 60
 done
 
+sleep 60
 umount /tmp/lan && rm -rf /tmp/lan
